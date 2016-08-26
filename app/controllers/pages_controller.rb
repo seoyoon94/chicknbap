b58d139d2cb77897bb8926e0e596a1311d16cc99
Seoyoon Park <seoyoon94@gmail.com>
2016-08-26 16:09:17 -0400

    Added top navbar and styles.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def menu
+  end
+
+  def contact
+  end
+
+  def hours
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get '/about', to: 'pages#about'
+  get '/menu', to: 'pages#menu'
+  get '/contact', to: 'pages#contact'
+  get '/hours', to: 'pages#hours'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
