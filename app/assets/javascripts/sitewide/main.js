04a4e3d1318e875eb2703ed0bb97e4495c4b6d4b
Seoyoon Park <seoyoon94@gmail.com>
2016-08-29 16:27:50 -0400

    Fixed-top header on scroll and layout of about page

---
+++ app/assets/javascripts/sitewide/main.js
@@ -0,0 +1,34 @@
+$.app = (function(app, $, undefined) {
+  app.fixNavOnScroll = function() {
+    let navbar = $('.navbar');
+    let headerHeight = navbar.height();
+    $(window).on('scroll', function() {
+      console.log(headerHeight);
+      if($(window).scrollTop() > headerHeight) {
+        navbar.addClass('navbar-is-fixed');
+      }
+      else {
+        navbar.removeClass('navbar-is-fixed');
+      }
+    });
+  };
+
+    /*  app.addAjax = function() {
+    $('a').on('click', function() {
+      $.ajax({
+        url: $(this).attr('href'),
+        method: 'GET'
+      })
+        .done(function(data, textStatus, xhr){
+          
+        })
+        .fail(function(xhr, textStatus, error) {
+        });
+    });
+  };*/
+  return app;
+})($.app || {}, jQuery);
+
+$(document).on('turbolinks:load', function() {
+  $.app.fixNavOnScroll();
+});
