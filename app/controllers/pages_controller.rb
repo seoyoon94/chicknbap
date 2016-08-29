74c7a1a31b937a5a6970758dcbbeaf41174372ca
Seoyoon Park <seoyoon94@gmail.com>
2016-08-29 17:41:23 -0400

    Added ajax functionality. Need to change urls on ajax visit.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,38 @@
 class PagesController < ApplicationController
+
   def index
+    respond_to do |format|
+      format.html
+      format.js { render layout: 'content' }
+    end
   end
 
   def about
+    respond_to do |format|
+      format.html
+      format.js { render layout: 'content' }
+    end
+
   end
 
   def menu
+    respond_to do |format|
+      format.html
+      format.js { render layout: 'content' }
+    end
   end
 
   def contact
+    respond_to do |format|
+      format.html
+      format.js { render layout: 'content' }
+    end
   end
 
   def hours
+    respond_to do |format|
+      format.html
+      format.js { render layout: 'content' }
+    end
   end
 end
